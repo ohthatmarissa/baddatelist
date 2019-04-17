0a853f41ef5762ecfa8cdea96b22766736709c20
Kyle Harrington <harrington.kyle@gmail.com>
2019-04-16 20:55:56 -0700

    add some hardcoded Reports to view-report.js

---
+++ js/view-report.js
@@ -0,0 +1,95 @@
+let chad = new Report (
+  neighborhood = "",
+  place = "",
+  contacted = "",
+  incident = "",
+  date = "",
+  name = "Chad Badboi",
+  car = "",
+  phone = "",
+  email = "",
+
+);
+let chadwick = new Report (
+  neighborhood = "",
+  place = "",
+  contacted = "",
+  incident = "",
+  date = "",
+  name = "Chadwick Weedick",
+  car = "",
+  phone = "",
+  email = ""
+);
+let chaddington = new Report (
+  neighborhood = "",
+  place = "",
+  contacted = "",
+  incident = "",
+  date = "",
+  name = "Chaddington Baddington",
+  car = "",
+  phone = "",
+  email = ""
+);
+let chad = new Report (
+  neighborhood = "",
+  place = "",
+  contacted = "",
+  incident = "",
+  date = "",
+  name = "Chadda Hound",
+  car = "",
+  phone = "",
+  email = ""
+);
+
+
+// UI logic
+let reportList = new ReportList();
+  reportList.addReport(chad);
+  reportList.addReport(chadwick);
+  reportList.addReport(chaddington);
+  reportList.addReport(chadda);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $("form#viewReport")submit(function(event){
+    event.preventDafault();
+  });
+});
